cd177337b0ee76e424a3ac7d68ae4ef704f7eb9b
Francisco Javier Martín Núñez <franjaviermn17100@gmail.com>
2020-05-06 19:59:56 +0200

    Añadida nueva ruta a nuestro programa principal, esta vez /listajuegos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, render_template
+from flask import Flask, abort, render_template, request
 import json
 #import os
 app=Flask(__name__)
@@ -13,5 +13,16 @@ def inicio():
 @app.route('/juegos', methods=["GET", "POST"])
 def juegos():
     return render_template('juegos.html')
+
+@app.route('/listajuegos', methods=["POST"])
+def listajuegos():
+    nombre_juego=request.form.get("nombre")
+    lista_nombres=[]
+    lista_desarrolladores=[]
+    for juegos in datos:
+        if str(juegos["nombre"]).startswith(nombre_juego):
+            lista_desarrolladores.append(juegos["desarrollador"])
+            lista_nombres.append(juegos["nombre"])
+    return render_template("listajuegos.html", lista_nombres=lista_nombres, lista_desarrolladores=lista_desarrolladores)
 
 app.run('0.0.0.0', debug=True)
