41b0924d7f7b3f7e6dee252ac805fc354f58dc86
Francisco Javier Martín Núñez <franjaviermn17100@gmail.com>
2020-05-06 13:28:18 +0200

    Añadida la nueva ruta para /juegos.

--- app.py
+++ app.py
@@ -10,4 +10,8 @@ with open("MSX.json") as fichero:
 def inicio():
     return render_template('inicio.html')
 
+@app.route('/juegos', methods=["GET", "POST"])
+def juegos():
+    return render_template('juegos.html')
+
 app.run('0.0.0.0', debug=True)
