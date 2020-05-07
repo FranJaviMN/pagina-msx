eda7dc2d6fd8f79c50a2b6ddca8adda6d76925bf
Francisco Javier Martín Núñez <franjaviermn17100@gmail.com>
2020-05-07 19:54:37 +0200

    Añadido los puertos para heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, abort, render_template, request
 import json
-#import os
+import os
 app=Flask(__name__)
 
 with open("MSX.json") as fichero:
@@ -41,4 +41,5 @@ def juego_id(identificador):
 
 
 
-app.run('0.0.0.0', debug=True)
+port=os.environ["PORT"]
+app.run('0.0.0.0', int(port), debug=False)
