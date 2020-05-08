afd4f1cad4cb25d0d14b6bb52aff03d69a6b34c6
Francisco Javier Martín Núñez <franjaviermn17100@gmail.com>
2020-05-08 14:15:44 +0200

    Fusionada las plantillas juegos.html y listajuegos.html, los resultados aparecen en una sola plantilla

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, abort, render_template, request
 import json
-import os
+#import os
 app=Flask(__name__)
 
 with open("MSX.json") as fichero:
@@ -12,24 +12,24 @@ def inicio():
 
 @app.route('/juegos', methods=["GET", "POST"])
 def juegos():
-    return render_template('juegos.html')
+    if request.method == "GET":
+        return render_template('juegos.html')
+    else:
+        nombre_juego=request.form.get("nombre")
+        lista_nombres=[]
+        lista_desarrolladores=[]
+        lista_ids=[]
+        for juegos in datos:
+            if nombre_juego == "":
+                lista_desarrolladores.append(juegos["desarrollador"])
+                lista_nombres.append(juegos["nombre"])
+                lista_ids.append(juegos["id"])
+            elif str(juegos["nombre"]).startswith(nombre_juego):
+                lista_desarrolladores.append(juegos["desarrollador"])
+                lista_nombres.append(juegos["nombre"])
+                lista_ids.append(juegos["id"])
+        return render_template("juegos.html", lista_nombres=lista_nombres, lista_desarrolladores=lista_desarrolladores, lista_ids=lista_ids)
 
-@app.route('/listajuegos', methods=["POST"])
-def listajuegos():
-    nombre_juego=request.form.get("nombre")
-    lista_nombres=[]
-    lista_desarrolladores=[]
-    lista_ids=[]
-    for juegos in datos:
-        if nombre_juego == "":
-            lista_desarrolladores.append(juegos["desarrollador"])
-            lista_nombres.append(juegos["nombre"])
-            lista_ids.append(juegos["id"])
-        elif str(juegos["nombre"]).startswith(nombre_juego):
-            lista_desarrolladores.append(juegos["desarrollador"])
-            lista_nombres.append(juegos["nombre"])
-            lista_ids.append(juegos["id"])
-    return render_template("listajuegos.html", lista_nombres=lista_nombres, lista_desarrolladores=lista_desarrolladores, lista_ids=lista_ids)
 
 @app.route('/juego/<int:identificador>')
 def juego_id(identificador):
@@ -41,5 +41,7 @@ def juego_id(identificador):
 
 
 
-port=os.environ["PORT"]
-app.run('0.0.0.0', int(port), debug=False)
+#port=os.environ["PORT"]
+#app.run('0.0.0.0', int(port), debug=False)
+
+app.run('0.0.0.0', debug=True)
