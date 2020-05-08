31c5d462f9f9a1e32d19aae772cfe4a645304f65
Francisco Javier Martín Núñez <franjaviermn17100@gmail.com>
2020-05-08 14:17:46 +0200

    Probar en heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, abort, render_template, request
 import json
-#import os
+import os
 app=Flask(__name__)
 
 with open("MSX.json") as fichero:
@@ -41,7 +41,7 @@ def juego_id(identificador):
 
 
 
-#port=os.environ["PORT"]
-#app.run('0.0.0.0', int(port), debug=False)
+port=os.environ["PORT"]
+app.run('0.0.0.0', int(port), debug=False)
 
-app.run('0.0.0.0', debug=True)
+#app.run('0.0.0.0', debug=True)
